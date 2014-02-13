1a46ddcfba6d7d24e5377c6f1c7bba2d90c0d955
Moritz Schulze <moritz.schulze@gmail.com>
2014-02-13 14:19:21 +0100

    Copied angular views to the dist folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,6 +47,7 @@ module.exports = function (grunt) {
                         dest: '<%= config.distFolder %>',
                         src: [
                             'index.html',
+                            'views/**',
                             'bower_components/**'
                         ]
                     }
