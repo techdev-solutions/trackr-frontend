5416e7bb92ea4a16b146a556ccd4ca1d17a5e712
Moritz Schulze <moritz.schulze@gmail.com>
2014-02-17 14:01:54 +0100

    Renamed ROLE_STAFF to ROLE_SUPERVISOR

--- src/modules/base/services/user.js
+++ src/modules/base/services/user.js
@@ -4,7 +4,7 @@ define([], function () {
         var user;
         var permissionLevels = {//TODO load this from the server
             ROLE_ADMIN: 0,
-            ROLE_STAFF: 1,
+            ROLE_SUPERVISOR: 1,
             ROLE_EMPLOYEE: 2
         };
         return {
@@ -25,7 +25,7 @@ define([], function () {
             /**
              * Check if the current user has a higher or equal authority than the argument
              * @param authority The authority to check for
-             * @returns {boolean} True if the user authority is higher, e.g. the user is ROLE_STAFF and the argument is ROLE_EMPLOYEE.
+             * @returns {boolean} True if the user authority is higher, e.g. the user is ROLE_SUPERVISOR and the argument is ROLE_EMPLOYEE.
              */
             userHasAuthority: function (authority) {
                 if (user.credentials.authorities.length === 0) {
