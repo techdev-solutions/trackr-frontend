e0b4b3d772a6a4f91fe2d2f78927077d8c8e68cf
Moritz Schulze <moritz.schulze@gmail.com>
2014-02-20 11:53:28 +0100

    Switched to ui-sref-active instead of own isActive function

--- src/modules/base/controllers/navigation.js
+++ src/modules/base/controllers/navigation.js
@@ -1,9 +1,6 @@
 define([], function () {
     'use strict';
     return ['$scope', '$location', 'base.services.user', function ($scope, $location, UserService) {
-        $scope.isActive = function (viewLocation) {
-            return (viewLocation === $location.path());
-        };
         $scope.user = UserService.getUser();
     }];
 });
--- test/modules/base/controllers/navigationSpec.js
+++ test/modules/base/controllers/navigationSpec.js
@@ -19,10 +19,6 @@ define(['app', 'angular-mocks'], function() {
             });
         }));
 
-        it('should have the isActive function in scope', function() {
-            expect(scope.isActive).toBeDefined();
-        });
-
         it('should put the active user into scope', function() {
             expect(UserService.getUser).toHaveBeenCalled();
             expect(scope.user).toBeDefined();
