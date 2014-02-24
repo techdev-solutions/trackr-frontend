489073efe477b218213b6e58e60920a46f06a2f0
Moritz Schulze <moritz.schulze@gmail.com>
2014-02-24 15:59:59 +0100

    TRACKR-7: Removal of contact persons in the frontend

--- src/modules/trackr/administration/controllers/companies/edit.js
+++ src/modules/trackr/administration/controllers/companies/edit.js
@@ -1,6 +1,6 @@
-define([], function() {
+define(['lodash'], function(_) {
     'use strict';
-    return ['$stateParams', '$scope', 'Restangular', function($stateParams, $scope, Restangular) {
+    return ['$stateParams', '$scope', 'Restangular', '$log', function($stateParams, $scope, Restangular, $log) {
         /**
          * Show or hide the form for a new contact person for this company
          * @param show true/false = show/hide
@@ -11,7 +11,6 @@ define([], function() {
 
         /**
          * POST the entered contact person to the server.
-         *
          * This method adds the company relationship to the contact person.
          */
         $scope.saveNewContactPerson = function() {
@@ -27,11 +26,32 @@ define([], function() {
             });
         };
 
+        /**
+         * Delete a contact person from this company by issuing a DELETE to the API
+         * @param contactPerson The contact person to delete
+         */
+        $scope.removeContactPerson = function(contactPerson) {
+            /*
+                We cant use the contactPerson object directly because it is tied to the company.
+                Restangular would execute DELETE '/api/companies/0/contactPersons/0' which is not what we want
+             */
+            Restangular.one('contactPersons', contactPerson.id).remove().then(function() {
+                _.remove($scope.contactPersons, function(cP) {
+                    return cP.id === contactPerson.id;
+                });
+            }, function(response) {
+                $log.error(response);
+            });
+        };
+
+        /*
+            Initialization of $scope objects
+         */
         $scope.contactPersonErrors = [];
         $scope.newContactPerson = {};
         $scope.doShowContactPersonForm(false);
         Restangular.allUrl('companies', '/api/companies/search/findByCompanyId').getList({companyId: $stateParams.id}).then(function(companies) {
-            //todo: why does spring return an array? the method signature is a single entity
+            //TODO: why does spring return an array? The method signature is a single entity.
             var company = companies[0];
 
             $scope.company = company;
