126e9f36bb32fc19ce12e7a4ba648ac0242528bc
Moritz Schulze <moritz.schulze@gmail.com>
2014-02-25 12:13:52 +0100

    TRACKR-23 - Create a multi purpose click-to-edit directive Click-to-edit possible. Does not work on nested properties yet (e.g. company.address.street)

---
+++ src/modules/shared/directives/bsEdit.js
@@ -0,0 +1,81 @@
+define([], function() {
+    'use strict';
+    /**
+     * Directive for a click-to-edit textfield
+     *
+     * Attributes:
+     * * entity: Reference to a restangularified entity
+     * * propertyName: the name of the property to display/edit
+     */
+    return [function() {
+        return {
+            restrict: 'E',
+            templateUrl: '/src/modules/shared/partials/bsEdit.tpl.html',
+            scope: {propertyName: '@', entity: '='},
+            link: function(scope, element) {
+                var oldValue;
+
+                //on click show the edit field
+                element.bind('click', function() {
+                    //save the old value so we can put it back if the user wants to abort
+                    oldValue = scope.entity[scope.propertyName];
+                    scope.edit = true;
+                    scope.$apply();
+                    //focus the cursor on the input element
+                    element.find('input')[0].focus();
+                });
+
+                //on focusout submit
+                element.bind('focusout', function () {
+                    //only submit if the value has changed so accidental clicks don't generate requests
+                    if(oldValue !== scope.entity[scope.propertyName]) {
+                        scope.submit();
+                        scope.$apply();
+                    } else {
+                        scope.edit = false;
+                        scope.$apply();
+                    }
+                });
+
+                //on escape cancel
+                element.bind('keydown', function(event) {
+                    if(event.which === 27) {
+                        scope.entity[scope.propertyName] = oldValue;
+                        scope.edit = false;
+                        scope.$apply();
+                    }
+                });
+            },
+            controller: ['$scope', 'Restangular', function($scope, Restangular) {
+                //Flag if the display form is showed
+                $scope.edit = false;
+                //Validation errors
+                $scope.errors = {};
+
+                $scope.hasError = function (property) {
+                    return $scope.errors[property] !== undefined;
+                };
+
+                $scope.errorText = function(property) {
+                    if($scope.hasError(property)) {
+                        return $scope.errors[property].defaultMessage;
+                    } else {
+                        return '';
+                    }
+                };
+
+                $scope.submit = function() {
+                    //We only want to submit the field we changed
+                    var patchObject = {};
+                    patchObject[$scope.propertyName] = $scope.entity[$scope.propertyName];
+                    //TODO: how to find out the name of the resource? entity.resource is not correct (e.g. credential, not credentials)
+                    Restangular.oneUrl('necessary', $scope.entity._links.self.href).patch(patchObject).then(function() {
+                        $scope.edit = false;
+                    }, function(response) {
+                        $scope.errors = response.data;
+                    });
+                };
+            }]
+        };
+    }];
+});
--- src/modules/shared/directives/directives.js
+++ src/modules/shared/directives/directives.js
@@ -1,8 +1,9 @@
-define(['modules/shared/directives/bsText'], function(bsText) {
+define(['modules/shared/directives/bsText', 'modules/shared/directives/bsEdit'], function(bsText, bsEdit) {
     'use strict';
     return {
         init: function(module) {
             module.directive('bsText', bsText);
+            module.directive('bsEdit', bsEdit);
         }
     };
 });
