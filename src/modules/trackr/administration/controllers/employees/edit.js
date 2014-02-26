dade06edc1ed43d7f55397a600afae542c62b4c2
Moritz Schulze <moritz.schulze@gmail.com>
2014-02-26 16:32:45 +0100

    TRACKR-11 - Activate user

--- src/modules/trackr/administration/controllers/employees/edit.js
+++ src/modules/trackr/administration/controllers/employees/edit.js
@@ -1,16 +1,30 @@
-define([], function () {
+define([], function() {
     'use strict';
-    return ['Restangular', '$scope', '$stateParams', '$controller', function (Restangular, $scope, $stateParams, $controller) {
+    return ['Restangular', '$scope', '$stateParams', '$controller', function(Restangular, $scope, $stateParams, $controller) {
         $controller('trackr.administration.controllers.employees.roles-base', {$scope: $scope});
 
+        //Contains the restangularified credential base object (i.e. URL = /api/credentials/xy not /api/employees/x/credential)
+        var credentialBase;
+
+        /**
+         * Switch the enabled flag in the credentials via a PATCH call to the API.
+         */
+        $scope.changeEnabled = function() {
+            var patch = {
+                enabled: !$scope.credential.enabled
+            };
+            credentialBase.patch(patch);
+        };
+
         /*
-            Initial load of employee and associated data.
+         Initial load of employee and associated data.
          */
         Restangular.one('employees', $stateParams.id).get().then(function(employee) {
             $scope.employee = employee;
             employee.one('credential').get().then(function(credential) {
                 $scope.credential = credential;
-                Restangular.one('credentials', credential.id).all('authorities').getList().then(function(authorities) {
+                credentialBase = Restangular.one('credentials', credential.id);
+                credentialBase.all('authorities').getList().then(function(authorities) {
                     $scope.credential.authorities = authorities;
                 });
             });
