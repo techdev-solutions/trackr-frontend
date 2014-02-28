06d100adfd3f572dee31a416b852bed18f86ea5a
Moritz Schulze <moritz.schulze@gmail.com>
2014-02-28 10:36:15 +0100

    TRACKR-26 - Create checkbox directive

---
+++ src/modules/shared/directives/bsCheckbox.js
@@ -0,0 +1,13 @@
+define([], function() {
+    'use strict';
+    return ['base.services.user', function(UserService) {
+        return {
+            restrict: 'E',
+            templateUrl: '/src/modules/shared/partials/bsCheckbox.tpl.html',
+            scope: {'checked': '=', 'change': '&', role: '@'},
+            controller: ['$scope', function($scope) {
+                $scope.editable = !$scope.role || UserService.userHasAuthority($scope.role);
+            }]
+        };
+    }];
+});
--- src/modules/shared/directives/directives.js
+++ src/modules/shared/directives/directives.js
@@ -1,9 +1,10 @@
-define(['modules/shared/directives/bsText', 'modules/shared/directives/bsEdit'], function(bsText, bsEdit) {
+define(['modules/shared/directives/bsText', 'modules/shared/directives/bsEdit', 'modules/shared/directives/bsCheckbox'], function(bsText, bsEdit, bsCheckbox) {
     'use strict';
     return {
         init: function(module) {
             module.directive('bsText', bsText);
             module.directive('bsEdit', bsEdit);
+            module.directive('bsCheckbox', bsCheckbox);
         }
     };
 });
