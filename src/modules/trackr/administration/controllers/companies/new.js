dbf14584cff88dbca3c43b449b3cc9513265783d
Moritz Schulze <moritz.schulze@gmail.com>
2014-03-11 10:29:28 +0100

    TRACKR-37 - Create DTOs for the creation of Company and Employee DTO for company/address creation

--- src/modules/trackr/administration/controllers/companies/new.js
+++ src/modules/trackr/administration/controllers/companies/new.js
@@ -1,45 +1,22 @@
 define([], function () {
     'use strict';
-    return ['$scope', 'Restangular', '$modalInstance', '$log', function($scope, Restangular, $modalInstance, $log) {
+    return ['$scope', 'Restangular', '$modalInstance', function($scope, Restangular, $modalInstance) {
         $scope.errors = {};
-        $scope.addressErrors = {};
         $scope.company = {};
         $scope.address = {};
 
         $scope.saveCompany = function() {
-            function persistCompany() {
-                // Set the address relationship.
-                $scope.company.address = $scope.address._links.self.href;
-                Restangular.all('companies').post($scope.company).then(function(company) {
-                    $modalInstance.close(company);
-                }, function(response) {
-                    $scope.errors = response.data;
-                });
-            }
-
-            /*
-                If the address was already persisted but there were validation errors when saving the company don't
-                save the address a second time.
-             */
-            if(!$scope.address._persisted) {
-                Restangular.all('addresses').post($scope.address).then(function(address) {
-                    $scope.addressErrors = {};
-                    $scope.address = address;
-                    $scope.address._persisted = true;
-                    persistCompany();
-                }, function(response) {
-                    $scope.addressErrors = response.data;
-                });
-            } else {
-                persistCompany();
-            }
+            Restangular.allUrl('companies', '/api/companies/createWithAddress').post({
+                company: $scope.company,
+                address: $scope.address
+            }).then(function(company) {
+                $modalInstance.close(company);
+            }, function(response) {
+                $scope.errors = response.data;
+            });
         };
 
         $scope.cancel = function() {
-            if($scope.address._persisted) {
-                $log.debug('Address was saved but company creation cancelled, deleting address');
-                $scope.address.remove();
-            }
             $modalInstance.dismiss();
         };
     }];
