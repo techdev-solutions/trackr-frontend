69d55ec5d58572bda914d2ccd900dd7da1f2439f
Moritz Schulze <moritz.schulze@gmail.com>
2014-03-14 15:42:53 +0100

    Moved two ifs into one

--- test/test-main.js
+++ test/test-main.js
@@ -1,10 +1,8 @@
 'use strict';
 var tests = [];
 for (var file in window.__karma__.files) {
-    if (window.__karma__.files.hasOwnProperty(file)) {
-        if (/spec\.js$/i.test(file) && ! /src\/vendor/i.test(file)) {
-            tests.push(file);
-        }
+    if (window.__karma__.files.hasOwnProperty(file) && /spec\.js$/i.test(file) && ! /src\/vendor/i.test(file)) {
+        tests.push(file);
     }
 }
 
