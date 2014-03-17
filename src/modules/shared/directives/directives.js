24aae87011927fedbea001d67367378ce2046235
Moritz Schulze <moritz.schulze@gmail.com>
2014-03-17 10:41:03 +0100

    TRACKR-58 - Create an autosize directive

--- src/modules/shared/directives/directives.js
+++ src/modules/shared/directives/directives.js
@@ -1,10 +1,16 @@
-define(['modules/shared/directives/bsText', 'modules/shared/directives/bsEdit', 'modules/shared/directives/bsCheckbox'], function(bsText, bsEdit, bsCheckbox) {
+define([
+    'modules/shared/directives/bsText',
+    'modules/shared/directives/bsEdit',
+    'modules/shared/directives/bsCheckbox',
+    'modules/shared/directives/autosize'
+], function(bsText, bsEdit, bsCheckbox, autosize) {
     'use strict';
     return {
         init: function(module) {
             module.directive('bsText', bsText);
             module.directive('bsEdit', bsEdit);
             module.directive('bsCheckbox', bsCheckbox);
+            module.directive('autosize', autosize);
         }
     };
 });
