e10f2ebee7069d5d3ef30633f2c487b535df0e47
Moritz Schulze <moritz.schulze@gmail.com>
2014-03-18 14:29:40 +0100

    TRACKR-54 - Request vacation Formatted start and end date before POSTing.

--- src/modules/trackr/employee/vacation/newController.js
+++ src/modules/trackr/employee/vacation/newController.js
@@ -4,14 +4,20 @@ define([], function() {
         var controller = this;
         $scope.vacationRequest = {};
 
+        controller.formatDate = function(date) {
+            return $filter('date')(date, 'yyyy-MM-dd');
+        };
+
         controller.emitSavedVacationRequest = function(vacationRequest) {
-            vacationRequest.startDate = $filter('date')(new Date(vacationRequest.startDate), 'yyyy-MM-dd');
-            vacationRequest.endDate = $filter('date')(new Date(vacationRequest.endDate), 'yyyy-MM-dd');
+            vacationRequest.startDate = controller.formatDate(new Date(vacationRequest.startDate));
+            vacationRequest.endDate = controller.formatDate(new Date(vacationRequest.endDate));
             $scope.$emit('newVacationRequest', vacationRequest);
         };
 
         $scope.submitVacationRequest = function(vacationRequest) {
             vacationRequest.employee = EmployeeService.getEmployeeHref();
+            vacationRequest.startDate = controller.formatDate(vacationRequest.startDate);
+            vacationRequest.endDate = controller.formatDate(vacationRequest.endDate);
             Restangular.all('vacationRequests').post(vacationRequest).then(function(response) {
                 controller.emitSavedVacationRequest(response);
             });
