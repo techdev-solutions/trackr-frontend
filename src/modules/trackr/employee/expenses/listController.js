18052c9bddca2ce5fc4294eba4e7c1a7ec89cc33
Moritz Schulze <moritz.schulze@gmail.com>
2014-04-11 15:00:34 +0200

    TRACKR-79 - Tracking of travel expenses New travel expenses are only creatable if the report is not accepted Translations Don't display the add travel expense form on accepted reports

--- src/modules/trackr/employee/expenses/listController.js
+++ src/modules/trackr/employee/expenses/listController.js
@@ -1,10 +1,14 @@
 define([], function() {
     'use strict';
     return ['$scope', 'Restangular', 'trackr.services.employee', '$state', function($scope, Restangular, EmployeeService, $state) {
-        Restangular.allUrl('travelExpenseReports', '/api/travelExpenseReports/search/findByEmployee')
+        Restangular.allUrl('travelExpenseReports', '/api/travelExpenseReports/search/findByEmployeeOrderByStatusAsc')
             .getList({employee: EmployeeService.getEmployee().id}).then(function(reports) {
                 $scope.reports = reports;
             });
+
+        $scope.acceptedAndRejected = function(travelExpense) {
+            return travelExpense.status === 'REJECTED' || travelExpense.status === 'ACCEPTED';
+        };
 
         /**
          * Add a new travel expense report and immediately switch to it.
