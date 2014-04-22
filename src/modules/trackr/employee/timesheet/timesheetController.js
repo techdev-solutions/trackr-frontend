886ba2a7db3368b0105e51a1746dd06401795bfc
Moritz Schulze <moritz.schulze@gmail.com>
2014-04-22 11:41:56 +0200

    TRACKR-86 - Show success message after working time has been saved Displayed message when saving working time.

--- src/modules/trackr/employee/timesheet/timesheetController.js
+++ src/modules/trackr/employee/timesheet/timesheetController.js
@@ -1,45 +1,48 @@
 define([], function() {
     'use strict';
-    return ['$scope', 'Restangular', 'trackr.services.employee', '$filter', function($scope, Restangular, EmployeeService, $filter) {
-        var controller = this;
-        var today = new Date();
-        $scope.date = today;
-        $scope.startTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 9, 0, 0);
-        $scope.endTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 17, 30, 0);
+    return ['$scope', 'Restangular', 'trackr.services.employee', '$filter', 'base.services.notification',
+        function($scope, Restangular, EmployeeService, $filter, NotificationService) {
+            var controller = this;
+            var today = new Date();
+            $scope.date = today;
+            $scope.startTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 9, 0, 0);
+            $scope.endTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 17, 30, 0);
 
-        $scope.errors = [];
+            $scope.errors = [];
 
-        $scope.getProjects = function(searchString) {
-            var search = '%' + searchString + '%';
-            return Restangular.allUrl('projects', 'api/projects/search/findByNameLikeOrIdentifierLikeOrderByNameAsc').getList({name: search, identifier: search});
-        };
-
-        controller.formatTime = function(date) {
-            return $filter('date')(date, 'HH:mm:ss');
-        };
-
-        controller.createWorkTimeEntity = function() {
-            var project;
-            if($scope.project) {
-                project = $scope.project._links.self.href;
-            }
-            return {
-                employee: EmployeeService.getEmployeeHref(),
-                project: project,
-                date: $scope.date,
-                startTime: controller.formatTime($scope.startTime),
-                endTime: controller.formatTime($scope.endTime),
-                comment: $scope.comment
+            $scope.getProjects = function(searchString) {
+                var search = '%' + searchString + '%';
+                return Restangular.allUrl('projects', 'api/projects/search/findByNameLikeOrIdentifierLikeOrderByNameAsc').getList({name: search, identifier: search});
             };
-        };
 
-        $scope.saveTime = function() {
-            var workTimeObject = controller.createWorkTimeEntity();
-            Restangular.all('workTimes').post(workTimeObject).then(function() {
-                $scope.errors = [];
-            }, function(response) {
-                $scope.errors = response.data.errors;
-            });
-        };
-    }];
+            controller.formatTime = function(date) {
+                return $filter('date')(date, 'HH:mm:ss');
+            };
+
+            controller.createWorkTimeEntity = function() {
+                var project;
+                if ($scope.project) {
+                    project = $scope.project._links.self.href;
+                }
+                return {
+                    employee: EmployeeService.getEmployeeHref(),
+                    project: project,
+                    date: $scope.date,
+                    startTime: controller.formatTime($scope.startTime),
+                    endTime: controller.formatTime($scope.endTime),
+                    comment: $scope.comment
+                };
+            };
+
+            $scope.saveTime = function() {
+                var workTimeObject = controller.createWorkTimeEntity();
+                Restangular.all('workTimes').post(workTimeObject).then(function() {
+                    $scope.errors = [];
+                    NotificationService.info('Working time saved.');
+                }, function(response) {
+                    $scope.errors = response.data.errors;
+                    NotificationService.error('Error saving working time');
+                });
+            };
+        }];
 });
