6e3b3923b8b765dfc6cbcb83d3f86b38d8cc7dc0
Moritz Schulze <moritz.schulze@gmail.com>
2014-06-10 16:18:54 +0200

    TRACKR-101 - Add invoice Displayed an info box when an invoice is created.

--- src/modules/invoices/newController.js
+++ src/modules/invoices/newController.js
@@ -1,36 +1,42 @@
-define([], function () {
+define([], function() {
     'use strict';
-    return ['$scope', '$modalInstance', 'Restangular', '$filter', function($scope, $modalInstance, Restangular, $filter) {
-        $scope.errors = [];
+    return ['$scope', '$modalInstance', 'Restangular', '$filter', 'base.services.notification',
+        function($scope, $modalInstance, Restangular, $filter, NotificationService) {
+            $scope.errors = [];
 
-        $scope.saveInvoice = function(invoice, debitor) {
-            invoice = invoice || {};
-            if(debitor) {
-                invoice.debitor = debitor._links.self.href;
-            }
-            invoice.invoiceState = 'OUTSTANDING';
-            Restangular.all('invoices').post(invoice)
-                .then(function(response) {
-                    $modalInstance.close(response);
-                }, function(response) {
-                    if(response.status === 409) {
-                        $scope.errors = [{
-                            entity: 'invoice',
-                            message: $filter('translate')('INVOICE.IDENTIFIER_CONFLICT'),
-                            property: 'identifier'
-                        }];
-                    } else {
-                        $scope.errors = response.data.errors;
-                    }
-                });
-        };
+            $scope.saveInvoice = function(invoice, debitor) {
+                invoice = invoice || {};
+                if (debitor) {
+                    invoice.debitor = debitor._links.self.href;
+                }
+                invoice.invoiceState = 'OUTSTANDING';
+                Restangular.all('invoices').post(invoice)
+                    .then(function(response) {
+                        $modalInstance.close(response);
+                        NotificationService.info(
+                            $filter('translate')('INVOICE.INVOICE_CREATED') + ' (' + $filter('translate')('INVOICE.' + response.invoiceState) + ')'
+                        );
+                    }, function(response) {
+                        if (response.status === 409) {
+                            $scope.errors = [
+                                {
+                                    entity: 'invoice',
+                                    message: $filter('translate')('INVOICE.IDENTIFIER_CONFLICT'),
+                                    property: 'identifier'
+                                }
+                            ];
+                        } else {
+                            $scope.errors = response.data.errors;
+                        }
+                    });
+            };
 
-        $scope.getCompanies = function(searchString) {
-            return Restangular.allUrl('companies', 'api/companies/search/findByNameLikeOrderByNameAsc').getList({name: '%' + searchString + '%'});
-        };
+            $scope.getCompanies = function(searchString) {
+                return Restangular.allUrl('companies', 'api/companies/search/findByNameLikeOrderByNameAsc').getList({name: '%' + searchString + '%'});
+            };
 
-        $scope.cancel = function() {
-            $modalInstance.dismiss();
-        };
-    }];
+            $scope.cancel = function() {
+                $modalInstance.dismiss();
+            };
+        }];
 });
