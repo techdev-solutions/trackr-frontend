cec375498db04d9e776e66d221910eed55da0f02
Moritz Schulze <moritz.schulze@gmail.com>
2014-06-13 11:57:41 +0200

    TRACKR-94 - Show Company and Debitor for Project

--- src/modules/trackr/administration/projects/editController.js
+++ src/modules/trackr/administration/projects/editController.js
@@ -1,6 +1,7 @@
 define([], function() {
     'use strict';
     return ['$stateParams', '$scope', 'Restangular', '$state', '$filter', function($stateParams, $scope, Restangular, $state, $filter) {
+        var controller = this;
         $scope.projectIdChanged = function() {
             $state.go('trackr.administration.projects.edit', {id: $scope.project.identifier});
         };
@@ -10,7 +11,7 @@ define([], function() {
          * @param response The HTTP response for the failed request.
          * @returns {*} An error array if the response status is 409, undefined otherwise.
          */
-        $scope.prjojectIdError = function(response) {
+        $scope.projectIdError = function(response) {
             if(response.status === 409) {
                 return [{
                     entity: 'project',
@@ -24,9 +25,41 @@ define([], function() {
         /*
             Initialization of $scope objects
          */
-        Restangular.allUrl('projects', 'api/projects/search/findByIdentifier').getList({identifier: $stateParams.id}).then(function(projects) {
+        Restangular.allUrl('projects', 'api/projects/search/findByIdentifier').getList({
+            identifier: $stateParams.id,
+            projection: 'withCompanyAndDebitor'
+        }).then(function(projects) {
             //TODO: why does spring return an array? The method signature is a single entity.
             $scope.project = projects[0];
         });
+
+        $scope.getCompanies = function(searchString) {
+            return Restangular.allUrl('companies', 'api/companies/search/findByNameLikeIgnoreCaseOrderByNameAsc').getList({name: '%' + searchString + '%'});
+        };
+
+        /**
+         * Change the company or debitor on a project.
+         *
+         * Sets a scope variable $scope.companySaved|$scope.debitorSaved to true after success.
+         * @param project The project
+         * @param company The company
+         * @param field 'company'|'debitor' according to what should be changed.
+         */
+        controller.changeCompanyOrDebitor = function(project, company, field) {
+            var queryParams = {};
+            $scope[field + 'Saved'] = false;
+            project.customPUT(company._links.self.href, field, queryParams, { 'Content-Type': 'text/uri-list'})
+                .then(function() {
+                    $scope[field + 'Saved'] = true;
+                });
+        };
+
+        $scope.changeCompany= function(project, company) {
+            controller.changeCompanyOrDebitor(project, company, 'company');
+        };
+
+        $scope.changeDebitor = function(project, debitor) {
+            controller.changeCompanyOrDebitor(project, debitor, 'debitor');
+        };
     }];
 });
