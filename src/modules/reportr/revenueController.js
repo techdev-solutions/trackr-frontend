fa0ab53e7343ee199ccca26a62bb62fbad54f0ff
Moritz Schulze <moritz.schulze@gmail.com>
2014-07-02 11:52:28 +0200

    TRACKR-163 - Add revenue reporting Added a generic time interval selector directive Used it for revenue reporting

--- src/modules/reportr/revenueController.js
+++ src/modules/reportr/revenueController.js
@@ -1,8 +1,17 @@
-define(['lodash'], function(_) {
+define(['lodash', 'moment'], function(_, moment) {
     'use strict';
     return ['Restangular', '$scope', function(Restangular, $scope) {
         var controller = this;
 
+        /**
+         * Takes a list of invoices and returns a map of the debitor name to the summed invoiceTotal
+         *
+         * [ {debitor: { name: 'd1' }, invoiceTotal: 3}, {debitor: { name: 'd2' }, invoiceTotal: 4}]
+         * ->
+         * { 'd1': 3, 'd2': 4 }
+         * @param invoices The invoices to convert
+         * @return {Object} A mapping of debitor names to numbers (sum of the invoices for that debitor)
+         */
         controller.mapInvoiceTotalsToDebitor = function(invoices) {
             return _.mapValues(
                 _.groupBy(invoices, function(invoice) {
@@ -16,14 +25,22 @@ define(['lodash'], function(_) {
             );
         };
 
-        Restangular.allUrl('invoices', 'api/invoices/search/findByCreationDateBetween')
-            .getList({
-                start: '2014-01-01',
-                end: '2014-06-30',
-                projection: 'withDebitor'
-            })
-            .then(function(invoices) {
-                $scope.invoices = controller.mapInvoiceTotalsToDebitor(invoices);
-            });
+        $scope.dateSelected = function(start, end) {
+            controller.loadInvoices(start, end);
+        };
+
+        controller.loadInvoices = function(start, end) {
+            Restangular.allUrl('invoices', 'api/invoices/search/findByCreationDateBetween')
+                .getList({
+                    start: start.getTime(),
+                    end: end.getTime(),
+                    projection: 'withDebitor'
+                })
+                .then(function(invoices) {
+                    $scope.invoices = controller.mapInvoiceTotalsToDebitor(invoices);
+                });
+        };
+
+        controller.loadInvoices(moment().startOf('month').toDate(), moment().endOf('month').toDate());
     }];
 });
--- src/modules/shared/directives/directives.js
+++ src/modules/shared/directives/directives.js
@@ -4,8 +4,9 @@ define([
     'modules/shared/directives/bsCheckbox',
     'modules/shared/directives/autosize',
     'modules/shared/directives/inlineDatepicker',
+    'modules/shared/directives/dateInterval',
     'modules/shared/directives/errorDisplay'
-], function(bsText, bsEdit, bsCheckbox, autosize, inlineDatepicker, errorDisplay) {
+], function(bsText, bsEdit, bsCheckbox, autosize, inlineDatepicker, dateInterval, errorDisplay) {
     'use strict';
     return {
         init: function(module) {
@@ -14,6 +15,7 @@ define([
             module.directive('bsCheckbox', bsCheckbox);
             module.directive('autosize', autosize);
             module.directive('inlineDatepicker', inlineDatepicker);
+            module.directive('dateInterval', dateInterval);
             module.directive('errorDisplay', errorDisplay);
         }
     };
