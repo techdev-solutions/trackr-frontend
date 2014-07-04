4bafa3a40e08d81b09ba2e8ace496e9cfdd8cb76
Moritz Schulze <moritz.schulze@gmail.com>
2014-07-04 14:29:25 +0200

    TRACKR-161 - Make the address book sortable Address book is now sortable. PaginationLoader supports changing of the sort oder. Typo fix in tableSort.js

--- src/modules/trackr/employee/address_book/addressBookController.js
+++ src/modules/trackr/employee/address_book/addressBookController.js
@@ -1,10 +1,18 @@
 define(['modules/shared/PaginationLoader'], function(PaginationLoader) {
     'use strict';
     return ['Restangular', '$scope', function(Restangular, $scope) {
-        var paginationLoader = new PaginationLoader(Restangular.all('address_book'), 'employees', '', $scope, 20);
+        var paginationLoader = new PaginationLoader(Restangular.all('address_book'), 'employees', 'lastName,asc', $scope, 20);
 
         $scope.setPage = function() {
             paginationLoader.loadPage($scope.employees.page.number);
+        };
+
+        $scope.sortBy = function(property, direction) {
+            var directionText = 'desc';
+            if(direction == -1) {
+                directionText = 'asc';
+            }
+            paginationLoader.loadPage($scope.employees.page.number, null, null, property + ',' + directionText);
         };
 
         //initially load all companies
