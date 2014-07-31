8a06532e156f448d43f1bbe83541950ee645f302
Moritz Schulze <moritz.schulze@gmail.com>
2014-07-31 16:34:10 +0200

    TRACKR-209 - Add debitor to travel expense report Supervisors see the debitor and the project, too. Supervisors can change the debitor and project.

--- src/modules/trackr/supervisor/expenses/editController.js
+++ src/modules/trackr/supervisor/expenses/editController.js
@@ -1,13 +1,18 @@
 define([], function() {
         'use strict';
-        return ['$scope', 'Restangular', 'trackr.services.travelExpenseReport', '$state', 'base.services.user', '$stateParams',
-            function($scope, Restangular, TravelExpenseReportService, $state, UserService, $stateParams) {
+        return ['$scope', 'Restangular', 'trackr.services.travelExpenseReport', '$state', 'base.services.user', '$stateParams', 'base.services.notification',
+            function($scope, Restangular, TravelExpenseReportService, $state, UserService, $stateParams, NotificationService) {
+                var controller = this;
+                var travelExpenseReportBase = Restangular.one('travelExpenseReports', $stateParams.id);
 
-                Restangular.one('travelExpenseReports', $stateParams.id)
+                travelExpenseReportBase
                     .get({
-                        projection: 'withEmployeeAndExpenses'
+                        projection: 'overview'
                     })
                     .then(function(report) {
+                        if(report.debitor) {
+                            controller.loadProjects(report.debitor);
+                        }
                         return report.all('comments').getList({
                             projection: 'withEmployee'
                         }).then(function(comments) {
@@ -47,11 +52,51 @@ define([], function() {
                     });
                 };
 
+                /**
+                 * Preprocessor for comment-section that adds the report to a comment.
+                 * @param {Object} comment The comment to edit
+                 * @return {Object} The comment with the report
+                 */
                 $scope.addReport = function(comment) {
                     comment.travelExpenseReport = $scope.report._links.self.href;
                     return comment;
                 };
 
+                $scope.getCompanies = function(searchString) {
+                    return Restangular.allUrl('companies', 'api/companies/search/findByNameLikeIgnoreCaseOrderByNameAsc')
+                        .getList({name: '%' + searchString + '%'});
+                };
+
+                controller.updateLink = function(name, href) {
+                    return travelExpenseReportBase.customOperation('put', name, {}, {'Content-Type': 'text/uri-list'}, href)
+                        .then(function() {
+                            NotificationService.info(name + ' changed.');
+                        }, function() {
+                            NotificationService.error('Error changing ' + name);
+                        });
+                };
+
+                $scope.selectCompany = function(company) {
+                    controller.deleteProject().then(function() {
+                        $scope.report.project = undefined;
+                        controller.updateLink('debitor', company._links.self.href);
+                    });
+                    controller.loadProjects(company);
+                };
+
+                controller.deleteProject = function() {
+                    return travelExpenseReportBase.one('project').remove();
+                };
+
+                controller.loadProjects = function(company) {
+                    Restangular.one('companies', company.id).all('projects').getList().then(function(projects) {
+                        $scope.projects = projects;
+                    });
+                };
+
+                $scope.selectProject = function(project) {
+                    controller.updateLink('project', project._links.self.href);
+                };
             }
         ];
     }
