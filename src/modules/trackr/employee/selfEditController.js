54698504e6c63947f5d657438642f16b7217aa0e
Moritz Schulze <moritz.schulze@techdev.de>
2015-06-03 16:39:08 +0200

    TRACKR-234 - Track employee's contact address Also made a copy of the sub-address object when editing a self-employee

--- src/modules/trackr/employee/selfEditController.js
+++ src/modules/trackr/employee/selfEditController.js
@@ -2,7 +2,7 @@ define(['lodash'], function(_) {
     'use strict';
     return ['createOrUpdateModal.userdata', '$scope', 'Restangular', function(employee, $scope, Restangular) {
         var controller = this;
-        $scope.employee = _.clone(employee, false);
+        $scope.employee = _.clone(employee, true);
 
         controller.saveEntity = function(employee) {
             var request = Restangular.oneUrl('employees', 'api/employees/' + employee.id + '/self');
